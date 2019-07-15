5b76f43c02e5d38e60228dcbd0a7d5ee66cfb522
Robert Ikanov <robert.ikanov@gmail.com>
2019-07-15 20:39:38 +0200

    [m] separating data and computing parts

--- board.cpp
+++ board.cpp
@@ -1,26 +1,28 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2019  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "board.h"
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>
 
-static int diff(const int& i1, const int& i2)
+Board::Board(const int& S)
+:_rows(S + 2)
+,_cols(S)
+,__move(nullptr)
 {
-    return i1 > i2 ? i1 - i2 : i2 -i1;
-}
-
-Board::Board(const int& S, const int& L)
-    :_bound(L)
-    ,_level(0)
-    ,_rows(S + 2)
-    ,_cols(S)
-    ,__collectionOfPlayer(new Node* [_cols + 1])  /*null-terminated array*/
-    ,__collectionOfProgram(new Node* [_cols + 1]) /*null-terminated array*/
-{
-    // n-th power values for heuristic deep sum
-    __pow[0] = 1;
-    for (int i = 1; i < _bound; ++i)
-    {
-        __pow[i] = __pow[i - 1] * S;
-    }
     // --- Create grid --- //
     // allocate size
     __theGrid = new Node* [_rows + 2];
@@ -55,10 +57,16 @@ Board::Board(const int& S, const int& L)
                 const int hor = (0 < dir && dir < 4) - (4 < dir);
                 toSet.next[dir] = &__theGrid[row + ver][col + hor];
             }
-            toSet.decreaseDepth = (diff((_rows + 1) / 2, row) <= 1 && diff((_cols + 1) / 2, col) <= 1) ? 0 : -1;
         }
     }
+    
+    // --- Collections of stones --- /
+    // allocate memory
+    __collectionOfPlayer = new Node* [_cols + 1] ;     /*null-terminated array*/
+    __collectionOfProgram = new Node* [_cols + 1];  /*null-terminated array*/
+    // default positions
     reset();
+    
     // set the middle field of the board as the place of victory
     _WINNER_SPOT = &__theGrid[(_rows + 1) / 2][(_cols + 1) / 2];
     // --------------- //
@@ -85,6 +93,8 @@ void Board::reset()
     __collectionOfProgram[_cols] = nullptr;
 
     // --- set step stacks for UI -- //
+    delete __move; // purify it
+    __move = new Step[MAX_NUMBER_OF_MOVES];
     _lastMove =  __move;
     // -------------- //
     srand(time(NULL));
@@ -94,288 +104,6 @@ Board::~Board()
 {
     delete[] __collectionOfPlayer;
     delete[] __collectionOfProgram;
-    std::cout<<"Finish"<<std::endl;
+    delete __move;
 }
 
-bool Board::makeStep(const int& id, const int& dir, Step& step)
-{
-    Node ** stone = nullptr;
-    if(id < 0)
-    {
-        stone = __collectionOfPlayer-(id+1);
-    }
-    return stone ? makeStep(stone, dir, step) : false;
-}
-
-bool Board::makeStep(Node ** stone, const int& dir, Step& step)
-{
-    if ( (*stone)->next[dir]->occupied != 0)
-    {
-        return false;
-    }
-    step.stone = stone;
-    step.place = *stone;
-    while(step.place->next[dir]->occupied == 0)
-    {
-        step.place = step.place->next[dir];
-    }
-    return true;
-}
-
-void Board::storeStep(Step S)
-{
-    S.revertableStep();
-    *(++_lastMove) = S;
-}
-
-void Board::undoStep()
-{
-    if (isStarted())
-    {
-        (_lastMove--)->revertableStep();
-    }
-}
-
-void Board::redoStep()
-{
-    if((_lastMove + 1)->isValid())
-    {
-        (++_lastMove)->revertableStep();
-    }
-}
-
-static inline void NL() {
-    std::cout << std::endl << '|';
-}
-void Board::show() const
-{
-    const char* separator = "---- ";
-    const char* empty     = "    |";
-    for(int row = _rows;  row > 0; --row)
-    {
-        NL();
-        for(int col = 1; col <= _cols; ++col)
-        {
-            std::cout<<separator;
-        }
-        NL();
-        for(int col = 1; col <= _cols; ++col)
-        {
-            const Node *field = &(__theGrid[row][col]);
-            const int CH = field->occupied;
-            if(field == _WINNER_SPOT && CH == 0)
-            {
-                std::cout<<" () |";
-                continue;
-            }
-            if(CH < 0)
-            {
-                std::cout<<' '<<'A'<<(-1*CH)<<" |";
-            }
-            else if(CH > 0)
-            {
-                std::cout<<' '<<'X'<<(CH)<<" |";
-            }
-            else
-            {
-                std::cout<<empty;
-            }
-        }
-    }
-    NL();
-    for(int col = 1; col <= _cols; ++col)
-    {
-        std::cout << separator;
-    }
-    NL();
-    std::cout << _level <<  ':'  <<_numberOfUsableSteps;
-}
-
-Board::Result Board::seek0(Board::Turn T, const bool& fast_check)
-{
-    Step test;
-    Node ** setOfStones = (T == YOUR_TURN) ? __collectionOfPlayer : __collectionOfProgram;
-    if (fast_check)
-    {
-        for (Node** nextStone = setOfStones; *nextStone; ++nextStone)
-        {
-            for (int direction = 0; direction < 8; ++direction)
-            {
-                if (makeStep(nextStone, direction, test) && isWinnerStep(test))
-                {
-                    return WON;
-                }
-            }
-        }
-    }
-    else
-    {
-        for (Node** nextStone = setOfStones; *nextStone; ++nextStone)
-        {
-            for (int direction = 0; direction < 8; ++direction)
-            {
-                if (makeStep(nextStone, direction, test) )
-                {
-                    return isWinnerStep(test) ? WON : UNSURE;
-                }
-            }
-        }
-    }
-    return UNSURE;
-}
-
-Board::Result Board::seek(Turn T, const int& depth, const bool& fast_check)
-{
-    if (depth == 0)
-    {
-        return seek0(T,fast_check);
-    }
-    bool fail = true;
-    Step test;
-    Node ** setOfStones = (T == YOUR_TURN) ? __collectionOfPlayer : __collectionOfProgram;
-    Turn nextTurn = (T == YOUR_TURN) ? MY_TURN : YOUR_TURN;
-    for (Node** nextStone = setOfStones; *nextStone; ++nextStone)
-    {
-        for (int direction = 0; direction < 8; ++direction)
-        {
-            if (makeStep(nextStone, direction, test))
-            {
-                if (isWinnerStep(test))
-                {
-                    return WON;
-                }
-                if (depth == 0)
-                {
-                    continue;
-                }
-                test.revertableStep();
-                const Result tip = seek(nextTurn, depth - 1, !fail);
-                test.revertableStep();
-                switch (tip)
-                {
-                case UNSURE:
-                    if (fast_check)
-                    {
-                        return UNSURE;
-                    }
-                    fail = false;
-                    break;
-                case LOST:
-                    return WON;
-                case WON:
-                    break;
-                default:
-                    break;
-                };
-            }
-        }
-    }
-    return fail ? LOST : UNSURE;
-}
-
-void Board::getUsableSteps()
-{
-    bool fastCheck = false;
-    _numberOfUsableSteps = 0;
-    for(int id =  getAllowedSteps() ; id > 0; --id)
-    {
-        Step test = __allowedSteps[id - 1]; 
-        if (isWinnerStep(test))
-        {
-            _numberOfUsableSteps = 1;
-            __usableSteps[0] = test;
-            return;
-        }
-        if (_level == 0)
-        {
-            __usableSteps[_numberOfUsableSteps++] = test;
-            continue;
-        }
-        test.revertableStep();
-        const Result tip = seek(_getStepsForAi ? YOUR_TURN : MY_TURN, _level - 1, fastCheck);
-        test.revertableStep();
-        switch (tip)
-        {
-        case UNSURE:
-            if ( !fastCheck)
-            {
-                __usableSteps[_numberOfUsableSteps++] = test;
-                fastCheck = (_numberOfUsableSteps > 2);
-            }
-            break;
-        case LOST:
-            _numberOfUsableSteps = 1;
-            __usableSteps[0] = test;
-            return;
-        case WON:
-            break;
-        default:
-            break;
-        }      
-    }
-}
-
-Step Board::getStep()
-{
-    Step bestFound;
-    Step rescueSteps[MAX_NUMBER_OF_STONES * 8];
-    int numberOfRescuedSteps = 0;
-    std::cout << std::endl;
-    for (_level = 0; _level <= _bound; _level += 1)
-    {
-        std::cout << _level << std::flush;
-        getUsableSteps();
-        std::cout << " .. " << std::flush;
-        if (_numberOfUsableSteps == 1)
-        {
-            bestFound = __usableSteps[0];
-            break; // don't think too much, an obviuos step is coming. Either winner or survivor
-        }
-        if (_numberOfUsableSteps == 0)
-        {
-            bestFound = rescueSteps[rand() % numberOfRescuedSteps];
-            break; // we lost. try to rescue the game
-        }
-        // continue the searching and store the last known 'non-loser' steps for rescueing
-        for (numberOfRescuedSteps = 0; numberOfRescuedSteps < _numberOfUsableSteps ; ++numberOfRescuedSteps)
-        {
-            rescueSteps[numberOfRescuedSteps] = __usableSteps[numberOfRescuedSteps];
-        }
-    }
-    if (_level > _bound /*obvious step not found*/ )
-    {
-        bestFound =  __usableSteps[rand() % _numberOfUsableSteps] ;
-    }
-    return bestFound;
-}
-
-int Board::getAllowedSteps()
-{
-    int count = 0;
-    Step next;
-    for (Node** nextStone = _getStepsForAi ?  __collectionOfProgram : __collectionOfPlayer; *nextStone; ++nextStone)
-    {
-        for (int direction = 0; direction < 8; ++direction)
-        {
-            if (makeStep(nextStone, direction, next))
-            {
-                __allowedSteps[count++] = next;
-            }
-        }
-    }
-    // shuffle
-    for (int shuffle = count; shuffle; )
-    {
-        const int i1 = rand() % count;
-        const int i2 = rand() % count;
-        if ( i1 != i2)
-        {
-            std::swap(__allowedSteps[i1], __allowedSteps[i2]);
-        }
-        else
-        {
-            --shuffle;
-        }
-    }
-    return count ;
-}
--- board.h
+++ board.h
@@ -1,72 +1,30 @@
-#ifndef BOARD_HEADER
-#define BOARD_HEADER
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2019  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <iostream>
+#ifndef BOARD_H
+#define BOARD_H
 
-static const int MAX_NUMBER_OF_MOVES = 200;
-static const int MAX_NUMBER_OF_STONES = 10;
-static const int MAX_HEURISTIC_DEPTH = 10;
-static const int WALL = -7;
-struct Node
-{
-    Node* next[8] = {};
-    int occupied = WALL;
-    int decreaseDepth = -1;
-    int row, col;
-};
-
-struct Step
-{
-    Node ** stone;
-    Node * place;
-
-public:
-    Step(): stone(nullptr), place(nullptr) {}
-    Step(Node ** s, Node * p): stone(s), place(p) {}
-    Step(const Step&) = default;
-    bool isValid() const {
-        return stone != nullptr && place != nullptr;
-    }
-    void revertableStep()
-    {
-        place->occupied = (*stone)->occupied; // put down
-        (*stone)->occupied = 0;              // and pick up
-        std::swap(*stone, place);
-    }
-
-    int decreaseDepth() const
-    {
-        return (*stone)->decreaseDepth;
-    }
-
-    void printData() const
-    {
-        printf("X%d->(%d, %d) ", (*stone)->occupied, place->row, place->col);
-    }
-};
+#include "str_step.h"
 
 class Board
 {
-public:
-    enum Result
-    {
-        LOST = -1,
-        UNSURE = 0,
-        WON = 1
-    };
-    enum Turn
-    {
-        MY_TURN,
-        YOUR_TURN
-    };
 protected:
-
-    int __pow[MAX_HEURISTIC_DEPTH];
-    bool _heuristic;
     const Node* _WINNER_SPOT;
-     int _bound;
-    int _level;
-
     // Board grid [rows+2][columns+2]
     Node ** __theGrid;
     const int _rows;
@@ -77,62 +35,11 @@ protected:
     Node ** __collectionOfProgram;
     
     // Store steps for UI (undo, redo)
-    Step __move[MAX_NUMBER_OF_MOVES], *_lastMove;
-
-    Step __usableSteps[MAX_NUMBER_OF_STONES * 8];
-    Step __allowedSteps[MAX_NUMBER_OF_STONES * 8];
-    bool _getStepsForAi = true;
-    int _numberOfUsableSteps;
-    Result seek(Turn T, const int& depth, const bool& fast_check = false);
-    Result seek0(Turn, const bool&);
-    
-    void getUsableSteps();
-    int getAllowedSteps();
-    
+    Step * __move, *_lastMove;
 public:
-    Board(const int&, const int&);
-    ~Board();
-    void swapOpponents() {
-        std::swap(__collectionOfPlayer, __collectionOfProgram);
-    }
-    const Node * getStone(const int & ID) const {
-        return ID > 0 ? __collectionOfProgram[ID - 1] : __collectionOfPlayer[-ID - 1];
-    }
-    const int& boundLevel() const {
-        return _bound;
-    }
-    void boundLevel(const int& bound) {
-        _bound = bound;
-    }
-    bool makeStep(const int& id, const int&, Step& );
-    bool makeStep(Node **, const int&, Step& );
-    bool isStarted() const {
-        return _lastMove != __move;
-    }
-    void storeStep(Step S);
-    void undoStep();
-    void redoStep();
-    void seekerSwap() {
-        _getStepsForAi = !_getStepsForAi;
-    }
-    void getStepsForAi(const bool& t) {
-        _getStepsForAi = t;
-    }
-    bool getStepsForAi() const {
-        return _getStepsForAi;
-    }
-    Step getStep();
-    bool isWinnerStep(const Step& S) const {
-        return S.place == _WINNER_SPOT;
-    }
-    
-    bool isFinished() const {
-        return _WINNER_SPOT->occupied;
-    }
-    
+    Board(const int&);
+    virtual ~Board();
     void reset();
-    void show() const;
 };
 
-#endif
-
+#endif // BOARD_H
